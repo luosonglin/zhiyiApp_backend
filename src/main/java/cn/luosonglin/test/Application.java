91bd0c8f536ea5fecf96e8031c822faa69acc4b8
luosonglin <luo1992athrun1102@qq.com>
2016-11-23 13:32:38 +0800

    修改SpringBoot默认生的设定成jar生成为war

--- src/main/java/cn/luosonglin/test/Application.java
+++ src/main/java/cn/luosonglin/test/Application.java
@@ -2,6 +2,8 @@ package cn.luosonglin.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,8 +13,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+        SpringApplication.run(Application.class, args);
+    }
+
+    //spring-boot提供的解决方案：生成tomcat服务器能管理的war包，而非内嵌的tomcat直接生成的jar包
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 }
