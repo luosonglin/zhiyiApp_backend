b824e39c98849ffd13bfcc89e205ebe133da0104
luosonglin <luo1992athrun1102@qq.com>
2016-11-24 10:54:46 +0800

    自定义属性、随机数、多环境配置等

--- src/main/java/cn/luosonglin/test/Application.java
+++ src/main/java/cn/luosonglin/test/Application.java
@@ -3,7 +3,7 @@ package cn.luosonglin.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.SpringBootServletInitializer;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,3 +24,4 @@ public class Application extends SpringBootServletInitializer {
         return application.sources(Application.class);
     }
 }
+
