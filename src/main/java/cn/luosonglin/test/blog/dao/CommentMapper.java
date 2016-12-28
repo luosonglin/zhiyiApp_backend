df055b1c0a9cddde3f5d4c1f7a3b5a39b83e2776
luosonglin <luo1992athrun1102@qq.com>
2016-12-28 14:56:58 +0800

    微博评论api添加user相关字段的返回

--- src/main/java/cn/luosonglin/test/blog/dao/CommentMapper.java
+++ src/main/java/cn/luosonglin/test/blog/dao/CommentMapper.java
@@ -1,6 +1,7 @@
 package cn.luosonglin.test.blog.dao;
 
 import cn.luosonglin.test.blog.entity.Comment;
+import cn.luosonglin.test.blog.entity.UserAndComment;
 import cn.luosonglin.test.domain.User;
 import org.apache.ibatis.annotations.*;
 
@@ -19,8 +20,9 @@ public interface CommentMapper {
     @Update("UPDATE blog_comment SET deleted_at=#{deleted_at} WHERE user_id =#{user_id} and blog_id = #{blog_id} and comment_id = #{comment_id}")
     void deleteComment(Map<String, Object> map);
 
-    @Select("SELECT * FROM blog_comment WHERE blog_id = #{blog_id}")
-    List<Comment> getComments(@Param("blog_id") Integer blog_id);
+//    @Select("SELECT * FROM blog_comment WHERE blog_id = #{blog_id}")
+    @Select("SELECT a.*, b.name, b.nick_name, b.user_pic, b.authen_status, b.company FROM blog_comment a,user_info b WHERE a.user_id = b.id and blog_id = #{blog_id}")
+    List<UserAndComment> getComments(@Param("blog_id") Integer blog_id);
 
     @Select("select user_id from blog_comment where id = #{comment_id}")
     Integer getUserId(@Param("comment_id") Integer comment_id);
---
+++ src/main/java/cn/luosonglin/test/blog/entity/UserAndComment.java
@@ -0,0 +1,118 @@
+package cn.luosonglin.test.blog.entity;
+
+import java.util.Date;
+
+/**
+ * Created by luosonglin on 28/12/2016.
+ */
+public class UserAndComment {
+    private Integer id;
+    private Integer userId;
+    private Integer blogId;
+    private Integer commentId;
+    private String content;
+    private Date createdAt;
+    private Date deletedAt;
+
+    private String name;
+    private String nickName;
+    private String company;
+    private String userPic;
+    private String authenStatus;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getBlogId() {
+        return blogId;
+    }
+
+    public void setBlogId(Integer blogId) {
+        this.blogId = blogId;
+    }
+
+    public Integer getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(Integer commentId) {
+        this.commentId = commentId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(Date deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getUserPic() {
+        return userPic;
+    }
+
+    public void setUserPic(String userPic) {
+        this.userPic = userPic;
+    }
+
+    public String getAuthenStatus() {
+        return authenStatus;
+    }
+
+    public void setAuthenStatus(String authenStatus) {
+        this.authenStatus = authenStatus;
+    }
+}
