30ed176d40be0b3bf77622b203ea5c5f93bc90d9
luosonglin <luo1992athrun1102@qq.com>
2017-03-16 23:21:26 +0800

    修改用于验证码和签到码随机数的工具类

--- src/main/java/cn/luosonglin/test/base/util/RandUtil.java
+++ src/main/java/cn/luosonglin/test/base/util/RandUtil.java
@@ -25,10 +25,11 @@ public class RandUtil {
         for (int i = 0; i < length; i++)
             result = result * 10 + array[i];
 
-        return result < 1000 ? rand(length, array) : String.valueOf(result);
+        return result < Math.pow(10,length-1) ? rand(length, array) : String.valueOf(result);
     }
 
     public static void main(String[] args) {
+        for (int i=0;i<100;i++)
         System.out.println(rand(4, array));
     }
 }
